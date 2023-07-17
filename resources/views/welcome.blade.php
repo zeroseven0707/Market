980b484796115a46405944eb6923764bbed04842
Muhamad Sihabudin <muhamadsihabudin@pop-os.localdomain>
2023-07-17 14:49:53 +0700

    commit pertama

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,7 +24,6 @@
          <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
              <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
                <p>{{ $item['namaVarian'] }}</p>
-               <i>{{ $item['hargaJual'] }}</i> <br>
                <img src="{{ $item['barcodeImage'] }}" alt="">
              </div>
          </div>
